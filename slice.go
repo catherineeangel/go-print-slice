6e2b601235b9834c0b859edee17c86307b77b472
Catherine Angel <catheriner40540@gmail.com>
2025-02-20 09:41:12 +0700

    add print utils

---
+++ slice.go
@@ -0,0 +1,19 @@
+package slice
+
+import "fmt"
+
+func Print1DSlice (slice []int) {
+	for _, val := range slice{
+		fmt.Print(val, " ")
+	}
+	fmt.Println()
+}
+
+func Print2DSlice (slice [][]int) {
+	for _, row:= range slice{
+		for _, val:=range row{
+			fmt.Print(val, " ")
+		}
+	}
+	fmt.Println()
+}
