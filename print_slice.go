bceeba0d7a250dea94157eb9df6bec37ff1e0806
Catherine Angel <catheriner40540@gmail.com>
2025-02-25 22:59:38 +0700

    update print

--- print_slice.go
+++ print_slice.go
@@ -13,7 +13,7 @@ func Print2DSlice(slice [][]int) {
 	for _, row := range slice {
 		for _, val := range row {
 			if val == 0 {
-				fmt.Print("*", "\t")
+				fmt.Print("\t")
 			} else {
 				fmt.Print(val, "\t")
 			}
