c415c54c7a79f55291972cee4563447a39b1d79a
Catherine Angel <catheriner40540@gmail.com>
2025-02-25 14:54:56 +0700

    update print

--- print_slice.go
+++ print_slice.go
@@ -2,18 +2,21 @@ package slice
 
 import "fmt"
 
-func Print1DSlice (slice []int) {
-	for _, val := range slice{
+func Print1DSlice(slice []int) {
+	for _, val := range slice {
 		fmt.Print(val, " ")
 	}
 	fmt.Println()
 }
 
-
-func Print2DSlice (slice [][]int) {
-	for _, row:= range slice{
-		for _, val:=range row{
-			fmt.Print(val, "\t")
+func Print2DSlice(slice [][]int) {
+	for _, row := range slice {
+		for _, val := range row {
+			if val == 0 {
+				fmt.Print("\t")
+			} else {
+				fmt.Print(val, "\t")
+			}
 		}
 		fmt.Println()
 	}
